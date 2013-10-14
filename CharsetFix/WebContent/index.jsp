3dff32447b9fb067f6d54c48145fd1b6b5f9b43f
nabilzhang <dumuqiao0317@gmail.com>
2013-10-14 21:06:19 +0800

    finish

---
+++ CharsetFix/WebContent/index.jsp
@@ -0,0 +1,45 @@
+<%@page import="java.net.URLDecoder"%>
+<%@page import="java.util.Map.Entry"%>
+<%@page import="java.util.Iterator"%>
+<%@page import="java.util.Map"%>
+<%@page import="me.nabil.app.charset.encoding.fix.CharsetDecoder;"%>
+<%@page language="java" contentType="text/html; charset=utf-8"%>
+<html>
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+</head>
+<h1>乱码转换</h1>
+<input type="text" name="name" id="name-text">
+<span>(输入乱码字符串，点击提交)</span>
+<button onclick="tijiao();">转换</button>
+<form id="name-form" action="" method="get">
+	<input type="hidden" name="name" id="name">
+</form>
+<script>
+	function tijiao() {
+		var xx = document.getElementById("name-text").value;
+		xx = encodeURIComponent(xx);
+		document.getElementById("name").value = xx;
+		document.getElementById("name-form").submit();
+
+	}
+</script>
+
+<%
+	if (request.getParameter("name") != null) {
+		String interactName = request.getParameter("name");
+		interactName = URLDecoder.decode(interactName, "utf-8");
+		System.out.print(interactName);
+		Map<String, String> map = CharsetDecoder.decode(interactName);
+		Iterator<Entry<String, String>> iterator = map.entrySet()
+				.iterator();
+		while (iterator.hasNext()) {
+			Entry<String, String> entry = iterator.next();
+			String outstr = "<p>" + entry.getKey() + " --- "
+					+ entry.getValue() + "</p>";
+			out.println(outstr);
+			System.out.println(outstr);
+		}
+	}
+%>
+</html>
---
+++ CharsetFix/src/me/nabil/app/charset/encoding/fix/CharsetDecoder.java
@@ -0,0 +1,88 @@
+package me.nabil.app.charset.encoding.fix;
+
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+/**
+ * 乱码字符传修正处理
+ * 
+ * @author nabil
+ *
+ */
+public class CharsetDecoder {
+	/**
+	 * 
+	 * 该方法根据输入的乱码进行处理，若乱码修正后满足包含（中文，字母，数字，-，_）
+	 * 则返回该编码名称和处理后的字符串组成的Map,输出字符串为utf-8
+	 * 
+	 * Map的key为{@link Charset#name()}
+	 * 
+	 * @param inputstr 乱码字符串
+	 * @return
+	 */
+	public static Map<String, String> decode(String inputstr) {
+		Map<String, String> retMap = new HashMap<String, String>();
+		Map<String, Charset> availableCharsets = Charset.availableCharsets();
+		Iterator<Entry<String, Charset>> iterator = availableCharsets
+				.entrySet().iterator();
+		while (iterator.hasNext()) {
+			Entry<String, Charset> entry = iterator.next();
+			String charsetName = entry.getKey();
+			Charset charset = entry.getValue();
+			if (Charset.isSupported(charsetName)) {
+				try {
+					String trans = new String(
+							inputstr.getBytes(charset.name()), "utf-8");
+					if (checkCN(trans)) {
+						System.out.println(charsetName + trans);
+						retMap.put(charsetName, trans);
+					}
+
+				} catch (Exception e) {
+					System.err.println(e);
+				}
+
+			}
+
+		}
+		return retMap;
+	}
+
+	private static boolean checkCN(String inputStr) {
+		Pattern p = Pattern.compile("^[\u4E00-\u9FA5a-zA-Z0-9_-]+$");
+		Matcher m = p.matcher(inputStr);
+		return m.matches();
+
+	}
+
+	public static void main(String args[]) {
+		String testInput = "娴眰-鐐瑰嚮璺宠浆";
+		Map<String, Charset> availableCharsets = Charset.availableCharsets();
+		Iterator<Entry<String, Charset>> iterator = availableCharsets
+				.entrySet().iterator();
+		while (iterator.hasNext()) {
+			Entry<String, Charset> entry = iterator.next();
+			String charsetName = entry.getKey();
+			Charset charset = entry.getValue();
+			if (Charset.isSupported(charsetName)) {
+				try {
+					String trans = new String(
+							testInput.getBytes(charset.name()), "utf-8");
+					if (checkCN(trans)) {
+						System.out.println(charsetName + trans);
+					} else {
+						// System.out.println(charsetName+trans);
+					}
+				} catch (Exception e) {
+					System.err.println(e);
+				}
+
+			}
+
+		}
+	}
+}
